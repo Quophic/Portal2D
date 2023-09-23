67e75857aee65a00ed80e6bfd38526549d47227f
quophic <quophic@qq.com>
2023-09-23 09:09:59 +0800

    后渲染近的传送门视野

--- Assets/Scripts/PortalView.cs
+++ Assets/Scripts/PortalView.cs
@@ -51,28 +51,38 @@ public class PortalView : MonoBehaviour
     {
         if (Material)
         {
+            float redDist = Vector3.Distance(controller.playerEye.position, controller.portalRed.transform.position);
+            float blueDist = Vector3.Distance(controller.playerEye.position, controller.portalBlue.transform.position);
+            
             RenderTexture tempTex = RenderTexture.GetTemporary(Screen.width, Screen.height);
-            Material.SetTexture("_PortalTex", controller.portalRed.ViewTexture);
-            Material.SetVector("_Plane1", GetViewPortPlane(controller.portalRed.Top, controller.playerEye.position, controller.portalRed.Bottom));
-            Material.SetVector("_Plane2", GetViewPortPlane(controller.portalRed.Bottom, controller.playerEye.position, controller.portalRed.Top));
-            Vector4 portalPlane = GetViewPortPlane(controller.portalRed.Top, controller.portalRed.Bottom, controller.playerEye.position);
-            portalPlane.z *= -1;
-            portalPlane.w *= -1;
-            Material.SetVector("_Plane3", portalPlane);
-            Graphics.Blit(source, tempTex, Material);
-            Material.SetTexture("_PortalTex", controller.portalBlue.ViewTexture);
-            Material.SetVector("_Plane1", GetViewPortPlane(controller.portalBlue.Top, controller.playerEye.position, controller.portalBlue.Bottom));
-            Material.SetVector("_Plane2", GetViewPortPlane(controller.portalBlue.Bottom, controller.playerEye.position, controller.portalBlue.Top));
-            portalPlane = GetViewPortPlane(controller.portalBlue.Top, controller.portalBlue.Bottom, controller.playerEye.position);
-            portalPlane.z *= -1;
-            portalPlane.w *= -1;
-            Material.SetVector("_Plane3", portalPlane);
-            Graphics.Blit(tempTex, destination, Material);
+            if(redDist > blueDist)
+            {
+                RenderPortalView(controller.portalRed, source, tempTex);
+                RenderPortalView(controller.portalBlue, tempTex, destination);
+            }
+            else
+            {
+                RenderPortalView(controller.portalBlue, source, tempTex);
+                RenderPortalView(controller.portalRed, tempTex, destination);
+            }
+            
             tempTex.Release();
         }
         else
         {
             Graphics.Blit(source, destination);
         }
+    }
+
+    private void RenderPortalView(Portal portal ,RenderTexture source, RenderTexture destination)
+    {
+        Material.SetTexture("_PortalTex", portal.ViewTexture);
+        Material.SetVector("_Plane1", GetViewPortPlane(portal.Top, controller.playerEye.position, portal.Bottom));
+        Material.SetVector("_Plane2", GetViewPortPlane(portal.Bottom, controller.playerEye.position, portal.Top));
+        Vector4 portalPlane = GetViewPortPlane(portal.Top, portal.Bottom, controller.playerEye.position);
+        portalPlane.z *= -1;
+        portalPlane.w *= -1;
+        Material.SetVector("_Plane3", portalPlane);
+        Graphics.Blit(source, destination, Material);
     }
 }
