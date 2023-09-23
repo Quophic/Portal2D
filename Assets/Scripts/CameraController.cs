e911881eaa84a2d766d6b74f95e677a58e63e3d7
quophic <quophic@qq.com>
2023-09-23 08:56:51 +0800

    主摄像机跟随

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerEye;
+    public float offsetFactor;
+    public Camera mainCam;
+    void Update()
+    {
+        Vector2 mousePos =  mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 newPos = Vector2.Lerp(playerEye.position, mousePos, offsetFactor);
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+    }
+}
