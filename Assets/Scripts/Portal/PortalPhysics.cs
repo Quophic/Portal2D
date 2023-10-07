8e76726df1cb4fd153073bc53629d30e089c26b8
quophic <quophic@qq.com>
2023-10-07 16:50:52 +0800

    经过传送门的射线检测

--- Assets/Scripts/Portal/PortalPhysics.cs
+++ Assets/Scripts/Portal/PortalPhysics.cs
@@ -7,7 +7,34 @@ static class PortalPhysics
     {
         Vector2 dir = end - start;
         float dis = dir.magnitude;
-        RaycastHit2D hit = Physics2D.Raycast(start, dir, dis, LayerMask.GetMask("PortalPlane"));
+        return ThroughPortal(start, dir, dis);
+    }
+    public static RaycastHit2D ThroughPortal(Vector2 origin, Vector2 dir, float dis)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(origin, dir, dis, LayerMask.GetMask("PortalPlane"));
+        return hit;
+    }
+
+    public static RaycastHit2D Raycast(Vector2 origin, Vector2 dir, float dis)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(origin, dir, dis, LayerMask.GetMask("Dynamic"));
+        if (hit)
+        {
+            Debug.DrawLine(origin, origin + dir.normalized * dis, Color.white, 1f);
+            return hit;
+        }
+        RaycastHit2D portalHit = ThroughPortal(origin, dir, dis);
+        if (portalHit)
+        {
+            Portal portal = portalHit.collider.GetComponent<Portal>();
+            Vector2 newOrigin = portal.TeleportMatrix.MultiplyPoint(portalHit.point);
+            Vector2 newDir = portal.TeleportMatrix.MultiplyVector(dir);
+            float newDis = dis - portalHit.distance;
+            hit = Physics2D.Raycast(newOrigin, newDir, newDis, LayerMask.GetMask("Dynamic"));
+
+            Debug.DrawLine(origin, origin + dir.normalized * portalHit.distance, Color.white, 1f);
+            Debug.DrawLine(newOrigin, newOrigin + newDir.normalized * newDis, Color.white, 1f);
+        }
         return hit;
     }
 
