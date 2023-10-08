7adb0bda8f4d859a1bef78e67c2417eb1cea293d
quophic <quophic@qq.com>
2023-10-08 12:31:17 +0800

    重构

--- Assets/Scripts/Portal/PortalPhysics.cs
+++ Assets/Scripts/Portal/PortalPhysics.cs
@@ -17,23 +17,27 @@ static class PortalPhysics
 
     public static RaycastHit2D Raycast(Vector2 origin, Vector2 dir, float dis, LayerMask layer)
     {
-        RaycastHit2D hit = Physics2D.Raycast(origin, dir, dis, layer);
-        if (hit)
-        {
-            Debug.DrawLine(origin, origin + dir.normalized * dis, Color.white, 1f);
-            return hit;
-        }
         RaycastHit2D portalHit = ThroughPortal(origin, dir, dis);
+        Portal portal = null;
+        RaycastHit2D hit;
         if (portalHit)
         {
-            Portal portal = portalHit.collider.GetComponent<Portal>();
-            Vector2 newOrigin = portal.TeleportMatrix.MultiplyPoint(portalHit.point);
-            Vector2 newDir = portal.TeleportMatrix.MultiplyVector(dir);
-            float newDis = dis - portalHit.distance;
-            hit = Physics2D.Raycast(newOrigin, newDir, newDis, layer);
-
-            Debug.DrawLine(origin, origin + dir.normalized * portalHit.distance, Color.white, 1f);
-            Debug.DrawLine(newOrigin, newOrigin + newDir.normalized * newDis, Color.white, 1f);
+            portal = portalHit.collider.GetComponent<Portal>();
+        }
+        if (portal != null && portal.controller.connected)
+        {
+            hit = Physics2D.Raycast(origin, dir, portalHit.distance, layer);
+            if (!hit)
+            {
+                Vector2 newOrigin = portal.TeleportMatrix.MultiplyPoint(portalHit.point);
+                Vector2 newDir = portal.TeleportMatrix.MultiplyVector(dir);
+                float newDis = dis - portalHit.distance;
+                hit = Physics2D.Raycast(newOrigin, newDir, newDis, layer);
+            }
+        }
+        else
+        {
+            hit = Physics2D.Raycast(origin, dir, dis, layer);
         }
         return hit;
     }
