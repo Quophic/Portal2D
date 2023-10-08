80912854e6207b1cca65481dbdb351bd9ea4613d
quophic <quophic@qq.com>
2023-10-08 12:17:11 +0800

    修复从背面穿过传送门也会被传送的问题

--- Assets/Scripts/Portal/PortalPhysics.cs
+++ Assets/Scripts/Portal/PortalPhysics.cs
@@ -12,6 +12,10 @@ static class PortalPhysics
     public static RaycastHit2D ThroughPortal(Vector2 origin, Vector2 dir, float dis)
     {
         RaycastHit2D hit = Physics2D.Raycast(origin, dir, dis, LayerMask.GetMask("PortalPlane"));
+        if(Vector2.Dot(hit.normal, dir) >= 0)
+        {
+            hit = new RaycastHit2D();
+        }
         return hit;
     }
 
