1f1d8d76384a581bc3020342d3ce04cda614432b
quophic <quophic@qq.com>
2023-10-17 07:29:17 +0800

    重构 关卡信息的管理

--- Assets/Editor/LevelTool.cs
+++ Assets/Editor/LevelTool.cs
@@ -7,37 +7,12 @@ using UnityEngine.SceneManagement;
 using Newtonsoft.Json;
 public class LevelTool
 {
-    public static readonly string PATH = Application.streamingAssetsPath + "/Level/LevelInfos.json";
     [MenuItem("Assets/Portal2D/Level/Add Current Level")]
     static void AddCurrentLevel()
     {
-        if (!File.Exists(PATH))
-        {
-            using (File.Create(PATH)) { }
-        }
-        List<LevelInfo> infos = null;
-        using (StreamReader sr = File.OpenText(PATH))
-        {
-            string readData = sr.ReadToEnd();
-            infos = JsonConvert.DeserializeObject<List<LevelInfo>>(readData);
-            
-            sr.Close();
-            sr.Dispose();
-        }
-        if (infos == null)
-        {
-            infos = new List<LevelInfo>();
-        }
+        LevelManager manager = new LevelManager();
         string sceneName = SceneManager.GetActiveScene().name;
-        infos.Add(new LevelInfo(sceneName, sceneName));
-        using (StreamWriter sw = new StreamWriter(PATH))
-        {
-            string data = JsonConvert.SerializeObject(infos);
-            sw.Write(data);
-            sw.Close();
-            sw.Dispose();
-        }
-
-        Debug.Log(" current open scene: " + SceneManager.GetActiveScene().path);
+        manager.AddLevel(new LevelInfo(sceneName, sceneName));
+        manager.Save();
     }
 }
--- Assets/Scripts/Level/LevelInfo.cs
+++ Assets/Scripts/Level/LevelInfo.cs
@@ -1,6 +1,60 @@
 using System;
+using System.IO;
 using UnityEngine;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
+public class LevelManager
+{
+    public static readonly string PATH = Application.streamingAssetsPath + "/Level/LevelInfos.json";
+    private List<LevelInfo> levelInfos = null;
+
+    public LevelManager()
+    {
+        Load();
+    }
+    public void Load()
+    {
+        if (!File.Exists(PATH))
+        {
+            using (File.Create(PATH)) { }
+        }
+        using (StreamReader sr = File.OpenText(PATH))
+        {
+            string readData = sr.ReadToEnd();
+            levelInfos = JsonConvert.DeserializeObject<List<LevelInfo>>(readData);
+
+            sr.Close();
+            sr.Dispose();
+        }
+        if(levelInfos == null)
+        {
+            levelInfos = new List<LevelInfo>();
+        }
+    }
+    public bool AddLevel(LevelInfo levelInfo)
+    {
+        foreach(var item in levelInfos)
+        {
+            if (item.scene.Equals(levelInfo.scene))
+            {
+                return false;
+            }
+        }
+        levelInfos.Add(levelInfo);
+        return true;
+    }
+    public void Save()
+    {
+        using (StreamWriter sw = new StreamWriter(PATH))
+        {
+            string data = JsonConvert.SerializeObject(levelInfos);
+            sw.Write(data);
+            sw.Close();
+            sw.Dispose();
+        }
+    }
+}
 
 
 [Serializable]
