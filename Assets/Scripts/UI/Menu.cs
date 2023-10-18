d941650b9927a75f00b2213f3c146126faa00c89
quophic <quophic@qq.com>
2023-10-18 19:56:06 +0800

    添加 返回主菜单选项

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -42,4 +42,8 @@ public class Menu : MonoBehaviour
     {        
         SceneManager.LoadScene(levelName);
     }
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
