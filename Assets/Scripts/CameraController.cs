fef54d1e01b9a670cf8fac94d7b8108f42cb90fd
quophic <quophic@qq.com>
2023-10-30 03:54:10 +0800

    修改 摄像机穿越传送门时的位置

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     public Transform playerEye;
     public float offsetFactor;
     public Camera mainCam;
+    public PortalTraveller playerTraveller;
     void Update()
     {
         SetCameraTransform();
@@ -16,6 +17,10 @@ public class CameraController : MonoBehaviour
     {
         Vector2 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 newPos = Vector2.Lerp(playerEye.position, mousePos, offsetFactor);
+        if(playerTraveller.teleported)
+        {
+            newPos = playerTraveller.closestPortal.linkedPortal.TeleportMatrix.MultiplyPoint(newPos);
+        }
         newPos.z = transform.position.z;
         transform.position = newPos;
     }
