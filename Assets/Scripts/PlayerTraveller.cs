b002c425f160823460e8e094e545df0636052850
quophic <quophic@qq.com>
2023-10-30 03:35:17 +0800

    重构 player 的传送方法

--- Assets/Scripts/PlayerTraveller.cs
+++ Assets/Scripts/PlayerTraveller.cs
@@ -8,6 +8,7 @@ public class PlayerTraveller : PortalTraveller
     public PortalController portalController;
     public CameraController CameraController;
     public PlayerController playerController;
+    public Transform playerEye;
     public override void EnterPortalThreshold(Portal portal)
     {
         base.EnterPortalThreshold(portal);
@@ -23,8 +24,11 @@ public class PlayerTraveller : PortalTraveller
         base.Teleport(teleportMatrix);
         rb2d.velocity = teleportMatrix.MultiplyVector(rb2d.velocity);
         rb2d.position = teleportMatrix.MultiplyPoint(rb2d.position);
-        Quaternion rotation = teleportMatrix.rotation;
-        transform.rotation = rotation * transform.rotation;
-        CameraController.SetCameraTransform();
+        Vector3 rotation = teleportMatrix.rotation.eulerAngles;
+        bool reversed = teleportMatrix.m22 < 0;
+        rb2d.rotation = reversed ? -rotation.z : rotation.z;
+        rotation.x = 0;
+        rotation.z = 0;
+        playerEye.localRotation = Quaternion.Euler(rotation) * playerEye.localRotation;
     }
 }
