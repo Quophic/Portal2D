bfd0504dee47dc3822fdc77c45f46614eba4f77d
quophic <quophic@qq.com>
2023-11-10 14:04:49 +0800

    为摄像机回正旋转添加缓动动画

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,25 +9,26 @@ public class CameraController : MonoBehaviour
     public CinemachineBrain brain;
     public PortalTraveller playerTraveller;
     public float rotSpeed;
+    private Quaternion targetRot;
+    private Quaternion oldRot;
+    private float t = 0;
+    private float rotTime = 0;
+    private AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
     private void Awake()
     {
         playerTraveller.OnTeleported += TeleportCamera;
     }
     void LateUpdate()
     {
-        SetCameraTransform();
-        
+        RotateCamera();
         brain.ManualUpdate();
     }
 
     public void TeleportCamera(Matrix4x4 m)
-    {
+    { 
         virtualCamera.ForceCameraPosition(m.MultiplyPoint(transform.position), m.rotation * transform.rotation);
-    }
-    public void SetCameraTransform()
-    {
-        Quaternion targetRot;
-        if(Vector3.Dot(transform.forward, Vector3.forward) > 0)
+        oldRot = transform.rotation;
+        if (Vector3.Dot(transform.forward, Vector3.forward) > 0)
         {
             targetRot = Quaternion.identity;
         }
@@ -35,6 +36,17 @@ public class CameraController : MonoBehaviour
         {
             targetRot = Quaternion.Euler(0, 180, 0);
         }
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotSpeed * Time.deltaTime);
+        rotTime = Quaternion.Angle(transform.rotation, targetRot);
+        t = 0;    
+    }
+    public void RotateCamera()
+    {
+        t += Time.deltaTime * rotSpeed;
+        float f = t;
+        if(rotTime != 0)
+        {
+            f = t / rotTime;
+        }
+        transform.rotation = Quaternion.Slerp(oldRot, targetRot, curve.Evaluate(f));
     }
 }
