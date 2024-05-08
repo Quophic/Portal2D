7aff44c4597b6af9a805c9bd4f8d0223960b2963
Quophic <quophic@qq.com>
2024-05-08 22:09:41 +0800

    摄像机自由移动

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System;
 
 public class CameraController : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera;
     public CinemachineBrain brain;
     public PortalTraveller playerTraveller;
+    public PlayerController playerController;
+    public float freeMoveSpeed;
     public float rotSpeed;
     private Quaternion targetRot;
     private Quaternion oldRot;
@@ -22,6 +25,27 @@ public class CameraController : MonoBehaviour
     {
         RotateCamera();
         brain.ManualUpdate();
+
+        if (Input.GetAxisRaw("FreeView") > 0)
+        {
+            playerController.enabled = false;
+            virtualCamera.enabled = false;
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.Locked;
+
+            var position = brain.transform.position;
+            position += brain.transform.right * Input.GetAxisRaw("Mouse X") * freeMoveSpeed * Time.unscaledDeltaTime;
+            position += brain.transform.up * Input.GetAxisRaw("Mouse Y") * freeMoveSpeed * Time.unscaledDeltaTime;
+            brain.transform.position = position;
+        }
+        else
+        {
+            playerController.enabled = true;
+            virtualCamera.enabled = true;
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
     }
 
     public void TeleportCamera(Matrix4x4 m)
@@ -44,7 +68,7 @@ public class CameraController : MonoBehaviour
     {
         t += Time.deltaTime * rotSpeed;
         float f = t;
-        if(rotTime != 0)
+        if (rotTime != 0)
         {
             f = t / rotTime;
         }
